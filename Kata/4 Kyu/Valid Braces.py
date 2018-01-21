d7b1004cd78724eeb8f39cddfc903610ea0dda61
ThiagoMArc <tf.arcanjo@gmail.com>
2018-01-21 02:05:18 -0200

    Change

--- Kata/4 Kyu/Valid Braces.py
+++
@@ -1,4 +0,0 @@
-def validBraces(string):
-    while len(string) > 0 and "()" in string or "[]" in string or "{}" in string:
-        string = string.replace("()","").replace("[]","").replace("{}","")
-    return len(string) == 0
